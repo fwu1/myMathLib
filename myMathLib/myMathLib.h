a9f66b8b99926650c8bb2ea0be21d0b4c43d56d6
Samuel Wu <samuel.wu@btnussfom0099.local>
2019-01-13 21:39:56 -0800

    Initial Commit

---
+++ myMathLib/myMathLib.h
@@ -0,0 +1,13 @@
+//
+//  myMathLib.h
+//  myMathLib
+//
+//  Created by Samuel Wu on 1/13/19.
+//  Copyright © 2019 Samuel Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface myMathLib : NSObject
+
+@end
---
+++ myMathLib/myMathLib.m
@@ -0,0 +1,13 @@
+//
+//  myMathLib.m
+//  myMathLib
+//
+//  Created by Samuel Wu on 1/13/19.
+//  Copyright © 2019 Samuel Wu. All rights reserved.
+//
+
+#import "myMathLib.h"
+
+@implementation myMathLib
+
+@end
